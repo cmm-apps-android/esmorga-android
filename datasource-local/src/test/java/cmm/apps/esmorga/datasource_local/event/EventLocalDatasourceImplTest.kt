2cd8bcc5bce8c2be27ec80afcdceaf391c18329e
Andres Lopez <andres.lopez@accenture.com>
2024-05-24 10:08:20 +0200

    Added another local datasource test and extracted out the fake dao

--- datasource-local/src/test/java/cmm/apps/esmorga/datasource_local/event/EventLocalDatasourceImplTest.kt
+++ datasource-local/src/test/java/cmm/apps/esmorga/datasource_local/event/EventLocalDatasourceImplTest.kt
@@ -8,11 +8,19 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import io.mockk.slot
 import kotlinx.coroutines.test.runTest
+import org.junit.After
 import org.junit.Assert
 import org.junit.Test
 import java.time.ZonedDateTime
 
 class EventLocalDatasourceImplTest {
+
+    private val fakeStorage = mutableListOf<String>()
+
+    @After
+    fun shutDown(){
+        fakeStorage.clear()
+    }
 
     @Test
     fun `given a working dao when events requested then events successfully returned`() = runTest {
@@ -28,28 +36,45 @@ class EventLocalDatasourceImplTest {
     }
 
     @Test
-    fun `given a working dao when events cached then events are stored successfully`() = runTest {
+    fun `given an empty storage when events cached then events are stored successfully`() = runTest {
         val localEventName = "LocalEvent"
-        val storage = mutableListOf<String>()
-        val slot = slot<List<EventLocalModel>>()
 
+        val sut = EventLocalDatasourceImpl(provideFakeDao())
+        sut.cacheEvents(listOf(EventDataModel(localEventName, ZonedDateTime.now())))
+        val result = sut.getEvents()
+
+        Assert.assertEquals(result.size, 1)
+        Assert.assertEquals(result[0].dataName, localEventName)
+    }
+
+    @Test
+    fun `given a storage with events when events cached then old events are removed and new events are stored successfully`() = runTest {
+        val localEventName = "LocalEvent"
+        fakeStorage.add("ShouldBeRemoved")
+
+        val sut = EventLocalDatasourceImpl(provideFakeDao())
+        sut.cacheEvents(listOf(EventDataModel(localEventName, ZonedDateTime.now())))
+        val result = sut.getEvents()
+
+        Assert.assertEquals(result.size, 1)
+        Assert.assertEquals(result[0].dataName, localEventName)
+    }
+
+    private fun provideFakeDao(): EventDao {
+        val slot = slot<List<EventLocalModel>>()
         val dao = mockk<EventDao>()
         coEvery { dao.getEvents() } coAnswers {
-            storage.map { name ->
+            fakeStorage.map { name ->
                 EventLocalModel(0, name, ZonedDateTime.now(), System.currentTimeMillis())
             }
         }
         coEvery { dao.insertEvent(capture(slot)) } coAnswers {
-            storage.addAll(slot.captured.map { event -> event.localName })
+            fakeStorage.addAll(slot.captured.map { event -> event.localName })
         }
         coEvery { dao.deleteAll() } coAnswers {
-            storage.clear()
+            fakeStorage.clear()
         }
 
-        val sut = EventLocalDatasourceImpl(dao)
-        sut.cacheEvents(listOf(EventDataModel(localEventName, ZonedDateTime.now())))
-        val result = sut.getEvents()
-
-        Assert.assertEquals(result[0].dataName, localEventName)
+        return dao
     }
 }
