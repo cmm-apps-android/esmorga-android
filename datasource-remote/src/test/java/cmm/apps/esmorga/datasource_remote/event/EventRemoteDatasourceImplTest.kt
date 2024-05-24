e9f8afe5b7b207c9a3f0a7f49b9c598033f8d243
Andres Lopez <andres.lopez@accenture.com>
2024-05-24 10:31:13 +0200

    Added remote datasource tests.

---
+++ datasource-remote/src/test/java/cmm/apps/esmorga/datasource_remote/event/EventRemoteDatasourceImplTest.kt
@@ -0,0 +1,51 @@
+package cmm.apps.esmorga.datasource_remote.event
+
+import cmm.apps.esmorga.datasource_remote.api.EventApi
+import cmm.apps.esmorga.datasource_remote.event.mock.EventRemoteMock
+import cmm.apps.esmorga.domain.error.EsmorgaException
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Assert
+import org.junit.Test
+import retrofit2.HttpException
+import retrofit2.Response
+
+class EventRemoteDatasourceImplTest {
+
+    @Test
+    fun `given a working api when events requested then event list is successfully returned`() = runTest {
+        val remoteEventName = "RemoteEvent"
+
+        val api = mockk<EventApi>(relaxed = true)
+        coEvery { api.getEvents() } returns EventRemoteMock.provideEventListWrapper(listOf(remoteEventName))
+
+        val sut = EventRemoteDatasourceImpl(api)
+        val result = sut.getEvents()
+
+        Assert.assertEquals(result[0].dataName, remoteEventName)
+    }
+
+    @Test
+    fun `given an api returning 500 when events requested then EsmorgaException is thrown`() = runTest {
+        val errorCode = 500
+
+        val api = mockk<EventApi>(relaxed = true)
+        coEvery { api.getEvents() } throws HttpException(Response.error<ResponseBody>(errorCode, "raw response body as string".toResponseBody("plain/text".toMediaTypeOrNull())))
+
+        val sut = EventRemoteDatasourceImpl(api)
+
+        val exception = try {
+            sut.getEvents()
+            null
+        } catch (exception: RuntimeException){
+            exception
+        }
+
+        Assert.assertTrue(exception is EsmorgaException)
+        Assert.assertEquals((exception as EsmorgaException).code, errorCode)
+    }
+}
---
+++ datasource-remote/src/test/java/cmm/apps/esmorga/datasource_remote/event/mock/EventRemoteMock.kt
@@ -0,0 +1,21 @@
+package cmm.apps.esmorga.datasource_remote.event.mock
+
+import cmm.apps.esmorga.datasource_remote.event.model.EventListWrapperRemoteModel
+import cmm.apps.esmorga.datasource_remote.event.model.EventRemoteModel
+
+
+object EventRemoteMock {
+
+    fun provideEventListWrapper(nameList: List<String>): EventListWrapperRemoteModel {
+        val list = provideEventList(nameList)
+        return EventListWrapperRemoteModel(list.size, list)
+    }
+
+    fun provideEventList(nameList: List<String>): List<EventRemoteModel> = nameList.map { name -> provideEvent(name) }
+
+    fun provideEvent(name: String): EventRemoteModel = EventRemoteModel(
+        remoteName = name,
+        remoteDate = "2030-12-31T23:59:59.000Z"
+    )
+
+}
