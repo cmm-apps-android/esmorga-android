de1d7cb31d12e58dbb1523c6419f319bdecdfffd
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2024-09-30 16:43:37 +0200

    change tempfile path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     }
     signingConfigs {
         create("release") {
-            val tmpFilePath = System.getProperty("user.home") + "/work/_temp/keystore/"
+            val tmpFilePath = "keystore/esmorga.keystore.jks" + "/work/_temp/keystore/"
 //            val tmpFilePath = "/Users/p.marino.cortes/EsmorgaAndroid/esmorga.keystore" + "/work/_temp/keystore/"
             val  allFilesFromDir = File(tmpFilePath).listFiles()
 
