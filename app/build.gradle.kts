4c35bf238be7281853e4a6718a0b4fd704eb1d91
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2024-09-30 10:38:45 +0200

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 ////                keyAlias = properties.getProperty("keyAlias")
 ////                keyPassword = properties.getProperty("keyPassword")
 ////                storePassword = properties.getProperty("storePassword")
-            storeFile = File("/Users/p.marino.cortes/EsmorgaAndroid/esmorga.keystore.jks")
+//            storeFile = File("/Users/p.marino.cortes/EsmorgaAndroid/esmorga.keystore.jks")
             keyAlias = "esmorga_android"
             keyPassword = "64eSMG9!VjLzk5Js"
             storePassword = "aX74&!Q5esmW6go"
