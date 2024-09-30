0771fa0dc1be68dfa41bdc3bce0ed3887b3e6f49
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2024-09-30 12:29:14 +0200

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,14 +31,14 @@ android {
 
             if (allFilesFromDir != null) {
                 val keystoreFile = allFilesFromDir.first()
-                keystoreFile.renameTo(file("esmorga.keystore.jks"))
+                keystoreFile.renameTo(file("keystore/esmorga.keystore.jks"))
             }
 //                storeFile = file(properties.getProperty("storeFile"))
 //                keyAlias = properties.getProperty("keyAlias")
 ////                keyPassword = properties.getProperty("keyPassword")
 ////                storePassword = properties.getProperty("storePassword")
 //            storeFile = File("/Users/p.marino.cortes/EsmorgaAndroid/esmorga.keystore.jks")
-            storeFile = file("esmorga.keystore.jks")
+            storeFile = file("keystore/esmorga.keystore.jks")
             keyAlias = "esmorga_android"
             keyPassword = "64eSMG9!VjLzk5Js"
             storePassword = "aX74&!Q5esmW6go"
