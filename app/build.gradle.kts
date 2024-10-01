943c739b622d896359a92264910d4b2daaab4a15
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2024-10-01 17:10:46 +0200

    action decode command

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     }
     signingConfigs {
         create("release") {
-            storeFile = file("../esmorga.keystore.jks")
+            storeFile = file("esmorga.keystore.jks")
             keyAlias = System.getenv("BUILD_KEY_ALIAS")
             keyPassword = System.getenv("BUILD_KEY_PASSWORD")
             storePassword = System.getenv("BUILD_STORE_PASSWORD")
