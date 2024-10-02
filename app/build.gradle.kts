f89064518b4f1507f0ded73659bbe765e53d34e4
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2024-10-02 14:41:39 +0200

    relative path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,9 +23,8 @@ android {
         }
     }
     signingConfigs {
-//        val keystoreFile: String by project
         create("release") {
-            storeFile = file("esmorga.keystore.jks")
+            storeFile = file("./esmorga.keystore.jks")
 //            storeFile = file("${System.getProperty("user.home")}/work/EsmorgaAndroid/EsmorgaAndroid/esmorga.keystore.jks")
             keyAlias = System.getenv("BUILD_KEY_ALIAS")
             keyPassword = System.getenv("BUILD_KEY_PASSWORD")
