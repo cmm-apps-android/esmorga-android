58cdbc2e2de6ea10d7a9f93931effbe5b5bc4bd1
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2024-10-02 09:43:59 +0200

    change signing config order

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("release")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
 
         }
     }
